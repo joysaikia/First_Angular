d3039b285bb954a458e7ddb0cd936de9024e67a0
unknown <joysekhars@gmail.com>
2023-09-15 15:53:28 +0530

    first angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'to-do-list';
+  list:any[]=[];
+  addTask(item:string)
+  {
+    console.warn(item);
+
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list);
+    
+  }
+  removeTask(id:number)
+  {
+    console.warn(id);
+    this.list=this.list.filter(item=>item.id!==id)
+
+  }
+  displayTask(item:string)
+  {
+    console.warn(item);
+  }
+}
